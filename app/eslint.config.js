4dfb513248989431ad0dbdd0c52820731b5f7d26
Pham Trung Hai <haipt@vuthao.com>
2026-01-07 14:21:22 +0700

    fix: update parserOptions in eslint configuration for improved TypeScript support

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -18,6 +18,11 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
+
   },
 ])
