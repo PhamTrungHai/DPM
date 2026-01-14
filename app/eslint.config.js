e5f5bd763dec530a46772a3d02b03b3cddbf0eea
Pham Trung Hai <haipt@vuthao.com>
2026-01-14 09:57:06 +0700

    chore: add Prettier configuration and update ESLint config to include react-query plugin

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -5,6 +5,7 @@ import eslintImport from 'eslint-plugin-import'
 import eslintPrettier from 'eslint-plugin-prettier'
 import eslintTurbo from 'eslint-plugin-turbo'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactQuery from '@tanstack/eslint-plugin-query'
 import onlyWarn from "eslint-plugin-only-warn";
 import tseslint, { parser } from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -16,6 +17,7 @@ export default defineConfig([
     plugins: {
       reactHooks,
       reactRefresh,
+      reactQuery,
       eslintImport,
       eslintPrettier,
       eslintTurbo,
